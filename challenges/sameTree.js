5bba1e92d56e7f5272b95474b78587931dbf72c0
Edwin <edwinx.naranjo@gmail.com>
2018-09-23 11:36:09 -0400

    create solution branch

--- challenges/sameTree.js
+++ challenges/sameTree.js
@@ -1,4 +1,4 @@
-// Build a function that takes two parameters (two binary trees data structures) and validate they are twins
+// Build a function that takes two parameters (two binary trees) and validate if they are twins
 // If they are twins then return true, else return false
 
 function sameTree(tree1, tree2){
@@ -6,11 +6,6 @@ function sameTree(tree1, tree2){
     return false
   }
   if( tree1.childNodes.length !== tree2.childNodes.length){
-    return false
-  }
-  if(tree1.childNodes !== tree2.childNodes.reverse()){
-    console.log(tree1.childNodes)
-    console.log(tree2.childNodes.reverse())
     return false
   }
   return true
